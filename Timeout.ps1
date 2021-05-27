ef1962175d7e521d275486044fdc1fc5bacce7c3
josegaleanop <82467631+josegaleanop@users.noreply.github.com>
2021-05-27 13:16:03 -0700

    Update Timeout.ps1

--- Timeout.ps1
+++ Timeout.ps1
@@ -1,16 +1,3 @@
-#requires -Version 2 
- 
-$maximumRuntimeSeconds = 500000 
- 
-$process = Start-Process -FilePath powershell.exe -ArgumentList '-Command Start-Sleep -Seconds 5000000' -PassThru 
- 
-try 
-{
-    $process | Wait-Process -Timeout $maximumRuntimeSeconds -ErrorAction Stop 
-    Write-Warning -Message 'Process successfully completed within timeout.' 
-}
-catch 
-{
-    Write-Warning -Message 'Process exceeded timeout, will be killed now.' 
-    $process | Stop-Process -Force 
-} 
+Write-Output "Timeout is for 10 seconds"
+Timeout /T 6000
+Write-Output "This line will be executed after 10 seconds if not interuptted"
